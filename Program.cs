6e11e5d3218f8c52cca158ee62b8d8e5ccf89e55
jona199608 <subjona@gmail.com>
2022-10-27 13:07:32 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// ejercicio 1 
+
+/*
+Console.Write("introdusca sus datos. ");
+Console.WriteLine("intorduce tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("intorduce tu apellido:");
+string apellido = Console.ReadLine();
+Console.WriteLine("intorduce tu edad:");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("hola, " + nombre + " " + apellido);
+Console.WriteLine("tienes " + edad);
+*/
+// ejercicio 2
+
+int puertas = 4;
+int ruedas = 4;
+string marca = "audi";
+string itv = "valido hasta 19";
+
+Console.WriteLine("coche: " + "puerta: " + puertas + " ruedas: " + ruedas + " marca: " + marca + " itv: " + itv);
+
+double peso = 25.5;
+double largo = 1.75;
+string material = "madera";
+string color = "marron oscuro";
+Console.WriteLine("mesa: " + "peso: " + peso + "kg" + " largo: " + largo + "cm" + " material: " + material + " color: " + color);
+
+// ejercicio 3
+
+int numero = 19;
+char a = 'B';
+
+Console.WriteLine("numero es mayor a 18:" + (numero >= 18));
+Console.WriteLine("a es igual a C:" + (a == 'C'));
+
+
+
+
+
